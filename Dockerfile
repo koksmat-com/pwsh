2168f066b5f0c652701e7bcc1d5774851032aa33
Niels Gregers Johansen <niels@jumpto365.com>
2023-06-01 16:15:32 +0200

    release

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,6 @@
 FROM mcr.microsoft.com/powershell:latest
 
 RUN pwsh -c "Install-Module -Name ExchangeOnlineManagement -force"
-RUN pwsh -c "Install-Module -Name Mdbc -force"
 
 WORKDIR /usr/src/app
 
--- src/getallmailboxes.ps1
+++ src/getallmailboxes.ps1
@@ -4,6 +4,7 @@ $EXCHORGANIZATION = $env:EXCHORGANIZATION
 $EXCHCERTIFICATEPATH = "$PSScriptRoot/cert.pfx"
 
 Write-Host "Connecting to $EXCHORGANIZATION"
+Write-Host "V0.2.0"
 
 Connect-ExchangeOnline -CertificateFilePath $EXCHCERTIFICATEPATH  -AppID $EXCHAPPID -Organization $EXCHORGANIZATION # -ShowBanner:$false #   -BypassMailboxAnchoring:$true
 
