09ed2537d10afbe30aa30ac59de5cbbebe64d84a
Niels Gregers Johansen <niels@jumpto365.com>
2023-06-08 17:23:55 +0200

    added logging statement

--- src/downloadRecipients.ps1
+++ src/downloadRecipients.ps1
@@ -1,7 +1,8 @@
 
 . $PSScriptRoot/.connect.ps1
-
-Get-Recipient -ResultSize Unlimited  
-| select Id,ExchangeObjectId,Guid,Alias,RecipientTypeDetails,EmailAddresses,DisplayName,DistinguishedName 
+Write-Host "Starting download"
+Get-ExoRecipient -ResultSize unlimited
+#| select Id,ExchangeObjectId,Guid,Alias,RecipientTypeDetails,EmailAddresses,DisplayName,DistinguishedName 
 | convertto-json  
 | Out-File -Encoding utf8NoBOM -FilePath "$($ENV:DATAOUT)/recipents.json"
+Write-Host "Completed download"
