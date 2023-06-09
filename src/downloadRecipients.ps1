c784a2a2f7cacb784a073a2c96d427c69357a2f5
Niels Gregers Johansen <niels@jumpto365.com>
2023-06-09 18:00:27 +0200

    bug: stored result in memory

--- src/downloadRecipients.ps1
+++ src/downloadRecipients.ps1
@@ -1,8 +1,8 @@
 
 . $PSScriptRoot/.connect.ps1
 Write-Output "Starting download"
-Get-ExoRecipient -ResultSize unlimited
+$recipients = Get-ExoRecipient -ResultSize unlimited
 #| select Id,ExchangeObjectId,Guid,Alias,RecipientTypeDetails,EmailAddresses,DisplayName,DistinguishedName 
-| convertto-json  
+convertto-json -inputobject $recipients
 | Out-File -Encoding utf8NoBOM -FilePath "$($ENV:DATAOUT)/recipents.json"
 Write-Output "Completed download"
